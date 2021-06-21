e050bdec10e0f53e561f37e9ec204861603f95f6
Jeremy Krovitz <jeremy@jeremykrovitz.com>
2021-06-21 14:47:57 -0500

    Test commit

---
+++ login-with-section-12/classes/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+class Config {
+
+    const DEVELOPMENT_URL = 'http://localhost:8888/login';
+
+    const PRODUCTION_URL = 'http://edwincodecollege.com/login_app';
+
+    const SMTP_HOST = 'smtp.mailtrap.io';
+
+    const SMTP_USER = 'd455be5b5ef677';
+
+    const SMTP_PASSWORD = '223b3cb4312524';
+
+    const SMTP_PORT = 2525;
+
+
+}
---
+++ login/test.php
@@ -0,0 +1,9 @@
+<?php
+
+require './vendor/autoload.php';
+
+require './functions/functions.php';
+
+
+send_email('edwin@gmail.com', 'testing UPDATED', 'SOME MESSAGE');
+
